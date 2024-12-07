2b59f59313704c005f95337cb74abee7e9588585
Waspsting <llfeimi@outlook.com>
2024-12-06 22:00:48 -0500

    added audio control and some sound effects to buttons

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -1,7 +1,27 @@
 extends Control
 
+var audio = AudioServer.get_bus_index("Master")
+
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 func _on_leave_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	$AudioStreamPlayer.play()
+	$optionsmenu.visible = true 
+
+
+func _on_back_pressed() -> void:
+	$AudioStreamPlayer.play()
+	$optionsmenu.visible = false
+	
+
+
+
+func _on_h_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(audio, linear_to_db($optionsmenu/HSlider.value))
+	$AudioStreamPlayer.play()
+	$optionsmenu/Label.text = "volume = " + str($optionsmenu/HSlider.value)
