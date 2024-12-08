5705840d798ab053f361550c394b2415c1458cd1
Waspsting <llfeimi@outlook.com>
2024-12-07 23:40:06 -0500

    SETTINGS SAVE NOWWWW
    HELL YEAH TOOK FOREVER

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -1,5 +1,6 @@
 extends Control
-
+var settings = Settings.new()
+var tempsettings = Settings.new()
 var audio = AudioServer.get_bus_index("Master")
 
 func _on_start_pressed() -> void:
@@ -8,23 +9,46 @@ func _on_start_pressed() -> void:
 func _on_leave_pressed() -> void:
 	get_tree().quit()
 
+# Settings bullshit from here on
 
 func _on_options_pressed() -> void:
 	$AudioStreamPlayer.play()
-	$optionsmenu.visible = true 
+	$optionsmenu.visible = true
+
 
 
 func _on_back_pressed() -> void:
 	$AudioStreamPlayer.play()
 	$optionsmenu.visible = false
-	
+	settings.loaddata()
+	AudioServer.set_bus_volume_db(audio, linear_to_db(settings.options["volume"]))
+	if settings.options["fullscreen"] == true :
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else : 
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+	$optionsmenu/HSlider.value = settings.options["volume"]
+	$optionsmenu/Label.text = "volume = " + str($optionsmenu/HSlider.value * 100) + "%"
+	$optionsmenu/CheckBox.button_pressed = settings.options["fullscreen"]
+
+
+func _on_apply_pressed() -> void:
+	$AudioStreamPlayer.play()
+	settings.options = tempsettings.options
+	settings.savedata()
+	AudioServer.set_bus_volume_db(audio, linear_to_db(settings.options["volume"]))
+	if settings.options["fullscreen"] == true :
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else : 
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 
 
-func _on_h_slider_drag_ended(value_changed: bool) -> void:
+func _on_h_slider_drag_ended(_value_changed: bool) -> void:
 	AudioServer.set_bus_volume_db(audio, linear_to_db($optionsmenu/HSlider.value))
 	$AudioStreamPlayer.play()
 	$optionsmenu/Label.text = "volume = " + str($optionsmenu/HSlider.value * 100) + "%"
+	tempsettings.options["volume"] = $optionsmenu/HSlider.value
 
 
 func fullscreen_on(toggled_on: bool) -> void:
@@ -33,3 +57,10 @@ func fullscreen_on(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else : 
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	tempsettings.options["fullscreen"] = toggled_on
+
+func _ready ():
+	settings.loaddata()
+	$optionsmenu/HSlider.value = settings.options["volume"]
+	$optionsmenu/Label.text = "volume = " + str($optionsmenu/HSlider.value * 100) + "%"
+	$optionsmenu/CheckBox.button_pressed = settings.options["fullscreen"]
---
+++ Scripts/saveload.gd
@@ -0,0 +1,23 @@
+class_name Settings
+extends Resource
+var options = {
+	"volume": 1,
+	"fullscreen": false}
+
+var savepath = "user://data.ini"
+func savedata ():
+	var config_file = ConfigFile.new()
+	config_file.set_value("options","volume", options["volume"])
+	config_file.set_value("options","fullscreen", options["fullscreen"])
+	var error = config_file.save(savepath)
+	if error: 
+		print(error)
+
+func loaddata ():
+	var config_file = ConfigFile.new()
+	var error = config_file.load(savepath)
+	if error: 
+		print(error)
+		return
+	options["volume"] = config_file.get_value("options", "volume", options["volume"])
+	options["fullscreen"] = config_file.get_value("options", "fullscreen", options["fullscreen"])
